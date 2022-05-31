8cce63f9d51cb4447988c12ca5c99a4ee245d027
ganden16 <alvindian85@gmail.com>
2022-05-31 11:04:37 +0700

    inisialisasi

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Lenovo\CobaLibrary;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+    public function sayHello(string $name): string
+    {
+        return  "Hello $name, my name is $this->name" . PHP_EOL;
+    }
+}
